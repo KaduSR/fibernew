1e35cf2e39e2398f1d0c17d2e2b608b653814c96
Kadu Ribeiro <kaduesr@gmail.com>
2025-05-19 08:59:29 -0300

    feat: adicionar dependência de react-icons e atualizar a seção de ofertas com novos planos e ícones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-/* import "@/styles/fonts.css";
-import "@/styles/styles1.css" */
+import "@/styles/fonts.css";
+import "@/styles/styles1.css"
 import "@/styles/globals.css";;
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
@@ -20,11 +20,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
-      <body className={inter.className}>
-        <Header />
+    <html lang="pt-BR"
+    className="min-h-screen__variable7d5794 __variable_31b03a __variable_b539cb light">
+      
+      <body className="overflow-x-hidden {inter.className}" >
+        <div className="relative flex min-h-screen flex-col">
+        <Header/>
         <main>{children}</main>
         <Footer />
+        </div>
       </body>
     </html>
   );
