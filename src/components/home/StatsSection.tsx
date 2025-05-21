eb6cf218de5b696dc8ea92ec5e7956d6bb9f350b
Kadu Ribeiro <kaduesr@gmail.com>
2025-05-21 13:51:21 -0300

    feat: melhorar acessibilidade e estrutura da seção Stats, ajustando classes e alinhamentos

--- src/components/home/StatsSection.tsx
+++ src/components/home/StatsSection.tsx
@@ -2,35 +2,35 @@
 
 export function StatsSection() {
   return (
-    <section className="bg-white py-12 md:py-16 dark:bg-black">
+    <section className="bg-white py-12 md:py-16 dark:bg-black items-center">
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-center items-center">
           {/* Stat 1 */}
-          <div className="flex flex-col items-center md:items-start">
+          <div className="flex flex-col items-center md:items-center justify-start ">
             <h3 className="text-3xl md:text-4xl font-bold text-gray-800 dark:text-white">
               1M<span className="text-primary">+</span>
             </h3>
-            <p className="mt-2 text-sm md:text-base text-gray-600 text-center md:text-left">
+            <p className="mt-2 text-sm md:text-base text-gray-600 text-center dark:text-white md:text-left">
               Mais de 1400 mil de clientes ativos e satisfeitos.
             </p>
           </div>
 
           {/* Stat 2 */}
-          <div className="flex flex-col items-center md:items-start">
-            <h3 className="text-3xl md:text-4xl font-bold text-gray-800">
+          <div className="flex flex-col items-center md:items-center">
+            <h3 className="text-3xl md:text-4xl font-bold text-gray-800 dark:text-white">
               1000<span className="text-primary">+</span>
             </h3>
-            <p className="mt-2 text-sm md:text-base text-gray-600 text-center md:text-left">
+            <p className="mt-2 text-sm md:text-base text-gray-600 text-center dark:text-white md:text-left">
               Está presente em mais de 1000 residências.
             </p>
           </div>
 
           {/* Stat 3 */}
-          <div className="flex flex-col items-center md:items-start">
-            <h3 className="text-3xl md:text-4xl font-bold text-gray-800">
+          <div className="flex flex-col items-center md:items-center">
+            <h3 className="text-3xl md:text-4xl font-bold text-gray-800 dark:text-white">
               2<span className="text-primary">+</span>
             </h3>
-            <p className="mt-2 text-sm md:text-base text-gray-600 text-center md:text-left">
+            <p className="mt-2 text-sm md:text-base text-gray-600 text-center md:text-left dark:text-white">
               A Fiber.NET em duas cidades do interior do Rio de Janeiro.
             </p>
           </div>
